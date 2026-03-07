85156cdcfa6210756c33b73e3721ea6d06838a1f
ZeNoobi <ctaaseen@gmail.com>
2026-03-06 19:04:23 -0800

    Commit 1

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/business_system'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def HelloWorld():
