45acadc915427089ce0a47dd26fcb6fdce1b8eeb
ZeNoobi <ctaaseen@gmail.com>
2026-03-09 00:22:31 -0700

    Created Front Ended with Inventory Working

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,28 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from controllers.inventory_controller import inventory_bp
+
+## npm run dev to start the frontend;
+## python app.py to start the backend;
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/business_system'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.register_blueprint(inventory_bp)
 
-db = SQLAlchemy(app)
 
-@app.route("/")
-def HelloWorld():
-    return "Hello World"
+#@app.route("/")
+#def HelloWorld():
+#    return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+##from flask_sqlalchemy import SQLAlchemy
+
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/business_system'
+##app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+##db = SQLAlchemy(app)    
+## name of application : Businexus
