ab1802ff1561d03f65d729278f33dbb90ce6747e
ZeNoobi <ctaaseen@gmail.com>
2026-04-04 21:57:25 -0700

    Navbar added to all pages

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -12,11 +12,21 @@ function Employees() {
   }, [])
 
   return (
-    <div className="p-6">
+    <div className="">
+    <div className=" text-blue-500 font-bold flex items-center justify-between bg-gray-800 p-6">
+        <button className="bg-gray-300 hover:bg-blue-700 hover:text-white text-gray-800 py-2 px-4 rounded hover:cursor-pointer">Dashboard</button>
+        <h3 className="text-6xl text-blue-500 font-bold">Businexus</h3>
+        <button className="bg-gray-300 hover:bg-blue-700 hover:text-white text-gray-800 py-2 px-4 rounded hover:cursor-pointer">Logout</button>
+      </div>
+      <div className="text-2xl font-bold mb-4 flex items-center justify-evenly bg-gray-700 border-b-2 border-black shadow-lg">
+        <div className="bg-gray-300 hover:bg-blue-700 hover:text-white text-gray-800 font-bold w-1/3 h-14 flex items-center justify-center hover:cursor-pointer">Branches</div>
+        <div className="bg-gray-300 hover:bg-blue-700 hover:text-white text-gray-800 font-bold w-1/3 h-14 flex items-center justify-center hover:cursor-pointer">Inventory</div>
+        <div className="bg-blue-500 text-white font-bold hover:cursor-pointer w-1/3 h-14 flex items-center justify-center">Employees</div>
+      </div>
 
       <div className="text-2xl font-bold mb-4 flex items-center gap-2 justify-evenly">
         <p>Employees</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-gray-300 hover:bg-blue-700 hover:text-white text-gray-800 font-bold py-2 px-4 rounded">
           Add Employee
         </button>
       </div>
